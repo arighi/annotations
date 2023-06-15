7628c0d2ebf49d147aee2ae42b372f12b8c327ab
Andrea Righi <andrea.righi@canonical.com>
2023-06-15 17:14:32 +0200

    packaging: provide a proper packaging (pip) for the tool
    Signed-off-by: Andrea Righi <andrea.righi@canonical.com>

---
+++ kconfig/version.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- mode: python -*-
+# version of annotations module
+# Copyright © 2022 Canonical Ltd.
+
+VERSION = "0.1"
+
+if __name__ == '__main__':
+    print(VERSION)
---
+++ setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+from setuptools import setup
+from kconfig.version import VERSION
+
+setup(
+    name='annotations',
+    version=VERSION,
+    author='Andrea Righi',
+    author_email='andrea.righi@canonical.com',
+    description='Manage Ubuntu kernel .config',
+    url='https://git.launchpad.net/~arighi/+git/annotations-tools',
+    license='GPLv2',
+    long_description=open(os.path.join(os.path.dirname(__file__),
+                                       'README.rst'), 'r').read(),
+    long_description_content_type="text/x-rts",
+    packages=['kconfig'],
+    entry_points = {
+        'console_scripts': [
+            'annotations = kconfig.run:main',
+        ]
+    },
+    scripts = [
+        'bin/sanitize-annotations',
+    ],
+    include_package_data=True,
+    classifiers=['Environment :: Console',
+                 'Intended Audience :: Developers',
+                 'Intended Audience :: System Administrators',
+                 'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+                 'Operating System :: POSIX :: Linux',
+             ],
+
+    zip_safe = False,
+)
