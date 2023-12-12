7339d062737a27815882c5e7b7fcfa8b1e9b8764
Andrea Righi <andrea.righi@canonical.com>
2023-12-12 17:03:52 +0100

    tests: add a new unit test to verify the arch override
    Signed-off-by: Andrea Righi <andrea.righi@canonical.com>

---
+++ tests/test_override.py
@@ -0,0 +1,15 @@
+import unittest
+
+from tests import utils
+
+from kconfig.annotations import Annotation
+
+
+class TestLoadAnnotations(unittest.TestCase):
+    def test_load(self):
+        for d in ("annotations.override.1",):
+            f = "tests/data/" + d
+            a = Annotation(f)
+            print(utils.to_dict(a))
+            r = utils.load_json(f + ".result")
+            self.assertEqual(utils.to_dict(a), r)
